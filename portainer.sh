073191311ede059c34e6bf8d5e16d5e6215b4441
tito18 <titoeocq18@hotmail.com>
2021-08-04 11:14:25 -0500

    First commit

---
+++ portainer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ver="portainer/portainer-ce:2.6.2-alpine"
+
+PS3='Choose an option to portainer: '
+opts=("stop" "start" "changeVER" "viewVER" "quit")
+
+select opt in "${opts[@]}"; do
+
+	case $opt in
+		"stop")
+			echo "To stop and remove portainer container"
+			docker stop portainer && docker rm portainer
+			break
+			;;
+		"start")
+			echo "To start portainer"
+			docker run -d -p 9090:8000 -p 9091:9000 --name=portainer --restart=always \
+			-v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data \
+			"${ver}"
+			break
+			;;
+		"changeVER")
+			docker stop portainer && docker rm portainer
+			echo "Introduce the portainer version that you want to deploy"
+			read ver
+			docker run -d -p 9090:8000 -p 9091:9000 --name=portainer --restart=always \
+            -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data \
+            "${ver}"
+			break
+			;;
+		"viewVER")
+			echo "The portainer version that running is ${ver}"
+			break
+			;;
+		"quit")
+			echo "User requested exit"
+			exit
+			;;
+		*) echo "invalid option $REPLY";;
+	esac
+done
